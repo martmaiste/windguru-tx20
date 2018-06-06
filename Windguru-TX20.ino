279e31d71fe66b24531a8474286139698fe9e4ee
ull <mart.maiste@gmail.com>
2018-06-06 10:06:17 +0300

    some major changes

--- Windguru-TX20.ino
+++ Windguru-TX20.ino
@@ -1,4 +1,4 @@
-atus/* 
+/* 
  *  Technoline TX20 Wind Sensor on ESP8266 for sending the wind data to Windguru
  *  Supports OLED display
  *  
@@ -22,8 +22,10 @@ atus/*
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+
 #include <MD5Builder.h>
 #include <U8g2lib.h>
 #include "Secrets.h"
@@ -45,10 +47,15 @@ const char* apipass = "***";
 
 volatile boolean TX20IncomingData = false;
 
-const unsigned long interval = 60000; // calculate wind speed and direction averages and send data to Windguru every 60 seconds
-unsigned long interval_count;         // interval counter
+const unsigned long interval = 60000;      // calculate wind speed and direction averages and send data to Windguru every 60 seconds
+unsigned long interval_count;              // interval counter
 
-char* dir[] = {                       // human readable wind directions are only shown on the OLED
+const unsigned long wifi_timeout = 30000;  // how long to try establishing the wifi connection
+unsigned long wifi_count;
+
+bool ok;                                   // was the http request successful or not
+
+char* dir[] = {                            // human readable wind directions are only shown on the OLED
   "N",  "NNE",  "NE",  "ENE",
   "E",  "ESE",  "SE",  "SSE",
   "S",  "SSW",  "SW",  "WSW",
@@ -74,9 +81,11 @@ U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
 WiFiUDP ntpUDP;      // epoch received from NTP is used for Windguru API salt
 NTPClient ntp(ntpUDP, "europe.pool.ntp.org", 7200, 60000);
+long salt;
+
+WiFiClient client;
 
 MD5Builder _md5;     // Windguru API requires some MD5 hashing
-
 String md5(String str) {
   _md5.begin();
   _md5.add(String(str));
@@ -91,12 +100,16 @@ void isTX20Rising() {
 }
 
 void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("\nTX20 ESP8266 Windguru");
+
+  client.setTimeout(5000);
+
   pinMode(DATAPIN, INPUT);
   pinMode(DTRPIN, OUTPUT);
+  digitalWrite(DTRPIN, HIGH);
   attachInterrupt(digitalPinToInterrupt(DATAPIN), isTX20Rising, RISING);
-  
-  Serial.begin(115200);
-  Serial.println("TX20 ESP8266 Windguru");
   
   #ifdef OLED
   u8g2.begin();
@@ -114,113 +127,141 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
 
-  unsigned long wifi_timeout = millis();
-  while (WiFi.status() != WL_CONNECTED) {
+  wifi_count = millis();
+  while ((WiFi.status() != WL_CONNECTED) && (millis() - wifi_timeout >= wifi_count)) {
     delay(500);
     Serial.print(".");
-    if (millis() - wifi_timeout >= 30000) {
-      ESP.reset();
-      delay(5000);
-    }
   }
-  Serial.println("");
-  
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println();
 
-  #ifdef OLED
-  u8g2.setCursor(0, 40);
-  u8g2.print(WiFi.localIP());
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    #ifdef OLED
+    u8g2.clearBuffer();
+    u8g2.setCursor(0, 12);
+    u8g2.print(WiFi.localIP());
+    u8g2.sendBuffer();
+    #endif
+    
+    while (not ntp.update()) {
+      delay(500);
+    }
+    
+    salt = ntp.getEpochTime();
+    Serial.print("Salt: ");
+    Serial.println(salt);
+    Serial.println(ntp.getFormattedTime());
+    #ifdef OLED
+    u8g2.setCursor(0, 44);
+    u8g2.print("NTP time");    
+    u8g2.setCursor(0, 60);
+    u8g2.print(ntp.getFormattedTime());
+    u8g2.sendBuffer();
+    #endif
+
+  } else {
+    Serial.println("No WiFi");
+    #ifdef OLED
+    u8g2.clearBuffer();
+    u8g2.setCursor(0, 12);
+    u8g2.print("No WiFi");
+    u8g2.sendBuffer();
+    #endif
+  }
+
+  // ready to receive data from the wind sensor
+  digitalWrite(DTRPIN, LOW);
   
+  #ifdef DEBUG
+  Serial.println("Waiting data");
+  #endif
+  #ifdef OLED
+  u8g2.setCursor(0, 28);
+  u8g2.print("Waiting data");
   u8g2.sendBuffer();
   u8g2.clearBuffer();
   #endif
-
-  digitalWrite(DTRPIN, LOW);
   
   interval_count = millis(); 
 }
 
 boolean readTX20() {
-    int bitcount=0;
+  int bitcount=0;
 
-    unsigned char chk;
-    unsigned int sa,sb,sd,se;
-    unsigned int sc,sf, pin;
+  unsigned char chk;
+  unsigned int sa,sb,sd,se;
+  unsigned int sc,sf, pin;
 
-    sa=sb=sd=se=0;
-    sc=0;sf=0;
-    String tx20RawDataS = "";
+  sa=sb=sd=se=0;
+  sc=0;sf=0;
+  String tx20RawDataS = "";
 
-    for (bitcount=41; bitcount>0; bitcount--) {
-      pin = (digitalRead(DATAPIN));
-      if (!pin) {
-        tx20RawDataS += "1";      
-      } else {
-        tx20RawDataS += "0";      
-      }
-      if ((bitcount==41-4) || (bitcount==41-8) || (bitcount==41-20)  || (bitcount==41-24)  || (bitcount==41-28)) {
-        tx20RawDataS += " ";
-      }      
-      if (bitcount > 41-5){
-        // start, inverted
-        sa = (sa<<1)|(pin^1);
-      } else
-      if (bitcount > 41-5-4){
-        // wind dir, inverted
-        sb = sb>>1 | ((pin^1)<<3);
-      } else
-      if (bitcount > 41-5-4-12){
-        // windspeed, inverted
-        sc = sc>>1 | ((pin^1)<<11);
-      } else
-      if (bitcount > 41-5-4-12-4){
-        // checksum, inverted
-        sd = sd>>1 | ((pin^1)<<3);
-      } else 
-      if (bitcount > 41-5-4-12-4-4){
-        // wind dir
-        se = se>>1 | (pin<<3);
-      } else {
-        // windspeed
-        sf = sf>>1 | (pin<<11);
-      } 
-          
-      delayMicroseconds(1220);    
-    }
-    
-    chk = ( sb + (sc&0xf) + ((sc>>4)&0xf) + ((sc>>8)&0xf) );chk&=0xf;
-    
-    delayMicroseconds(2000);  // just in case
-    TX20IncomingData = false;  
-
-    if (chk == sd) {          // if checksum seems to be ok
-      quarter = dir[sb];
-      dir_now  = float(sb)*22.5;
-      wind_now = float(sc)/10;
-  
-      if (wind_now > wind_max) wind_max = wind_now;
-      if (wind_min == 0) wind_min = wind_now;
-      if (wind_now < wind_min) wind_min = wind_now;
-      
-      wind_num++;
-      wind_sum += wind_now;
-      wind_avg = wind_sum / wind_num;
-
-      dir_num++;
-      dir_sum += dir_now;
-      wind_dir = dir_sum / dir_num;
-
-      return true;
+  for (bitcount=41; bitcount>0; bitcount--) {
+    pin = (digitalRead(DATAPIN));
+    if (!pin) {
+      tx20RawDataS += "1";      
     } else {
-      return false;      
+      tx20RawDataS += "0";      
     }
+    if ((bitcount==41-4) || (bitcount==41-8) || (bitcount==41-20)  || (bitcount==41-24)  || (bitcount==41-28)) {
+      tx20RawDataS += " ";
+    }      
+    if (bitcount > 41-5){
+      // start, inverted
+      sa = (sa<<1)|(pin^1);
+    } else
+    if (bitcount > 41-5-4){
+      // wind dir, inverted
+      sb = sb>>1 | ((pin^1)<<3);
+    } else
+    if (bitcount > 41-5-4-12){
+      // windspeed, inverted
+      sc = sc>>1 | ((pin^1)<<11);
+    } else
+    if (bitcount > 41-5-4-12-4){
+      // checksum, inverted
+      sd = sd>>1 | ((pin^1)<<3);
+    } else 
+    if (bitcount > 41-5-4-12-4-4){
+      // wind dir
+      se = se>>1 | (pin<<3);
+    } else {
+      // windspeed
+      sf = sf>>1 | (pin<<11);
+    } 
+        
+    delayMicroseconds(1220);    
+  }
+  
+  chk = ( sb + (sc&0xf) + ((sc>>4)&0xf) + ((sc>>8)&0xf) );chk&=0xf;
+
+  delayMicroseconds(2000);  // just in case
+  
+  TX20IncomingData = false;  
+
+  if ((chk == sd) && (sc < 400)) {          // if checksum seems to be ok and wind speed below 40 m/s
+    quarter = dir[sb];
+    dir_now  = float(sb)*22.5;
+    wind_now = float(sc)/10;
+
+    if (wind_now > wind_max) wind_max = wind_now;
+    if ((wind_now < wind_min) || (wind_min == 0)) wind_min = wind_now;
+    
+    wind_num++;
+    wind_sum += wind_now;
+    wind_avg = wind_sum / wind_num;
+
+    dir_num++;
+    dir_sum += dir_now;
+    wind_dir = dir_sum / dir_num;
+
+    return true;
+  } else {
+    return false;      
+  }
 }
 
-/*
- * LOOP
- */
 void loop() {
   if (TX20IncomingData) {
     if (readTX20()) {
@@ -228,10 +269,11 @@ void loop() {
       Serial.print(" ");
       Serial.print(wind_dir);
       Serial.print("° ");
+      Serial.print(wind_now);
+      Serial.print(" ");
       Serial.print(wind_min);
       Serial.print("/");
-      Serial.print(wind_avg);
-  
+      Serial.print(wind_avg);  
       Serial.print("/");
       Serial.println(wind_max);
 
@@ -248,57 +290,93 @@ void loop() {
       
       u8g2.setFont(u8g2_font_inb30_mf);
       u8g2.setCursor(0, 60);
-      u8g2.print(wind_avg);
+      u8g2.print(wind_now);
       u8g2.print("  ");      
       u8g2.sendBuffer();
       #endif
     }
   }
 
-  if (millis() - interval_count >= interval ) {
-    ntp.update();
-    long epoch = ntp.getEpochTime();
+  if (millis() - interval_count >= interval) {
+    if (not wind_num) {
+        #ifdef DEBUG
+        Serial.println("No wind data");
+        #endif
+        #ifdef OLED
+        u8g2.clearBuffer();
+        u8g2.setFont(u8g2_font_helvB12_tf);
+        u8g2.setCursor(0, 12);
+        u8g2.print("No wind data");
+        u8g2.sendBuffer();
+        #endif
+    } else {
 
-    String url = "/upload/api.php?uid=" + String(uid);
-    url += "&salt="             + String(epoch);
-    url += "&hash="             + md5(String(epoch)+String(uid)+String(apipass));
-    url += "&wind_avg="         + String(wind_avg * 1.943844);
-    url += "&wind_min="         + String(wind_min * 1.943844);
-    url += "&wind_max="         + String(wind_max * 1.943844);
-    url += "&wind_direction="   + String(wind_dir);
-
-    #ifdef DEBUG
-    Serial.println(url);
-    #endif
-    WiFiClient client;
-    if (client.connect(host, 80)) {
-      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" + 
-                 "Connection: close\r\n\r\n");
-      delay(500);
-    }
-
-    bool ok = false;
-    while(client.available()) {
-      String line = client.readStringUntil('\n');   // read until the end of header and body 
-      #ifdef DEBUG
-      Serial.println(line);
-      #endif
-      if (line.startsWith("OK")) {                  // Server confirms that the request was "OK"
-        ok = true;
-        Serial.println("Server accepted the data");
+      // reconnect to wifi if needed
+      wifi_count = millis();
+      while ((WiFi.status() != WL_CONNECTED) && (millis() - wifi_timeout >= wifi_count)) {
+        delay(500);
+        Serial.print(".");
       }
-    }
+  
+      String url = "/upload/api.php?uid=" + String(uid);
+      url += "&salt="             + String(++salt);
+      url += "&hash="             + md5(String(salt)+String(uid)+String(apipass));
+      url += "&wind_avg="         + String(wind_avg * 1.943844);
+      url += "&wind_min="         + String(wind_min * 1.943844);
+      url += "&wind_max="         + String(wind_max * 1.943844);
+      url += "&wind_direction="   + String(wind_dir);
+  
+      #ifdef DEBUG
+      Serial.println(url);
+      #endif
+      
+      #ifdef OLED
+      u8g2.clearBuffer();
+      u8g2.setFont(u8g2_font_helvB12_tf);
+      u8g2.setCursor(0, 12);
+      u8g2.print("Send ");
+      u8g2.sendBuffer();
+      #endif
+      
+      ok = false;
+      
+      if (client.connect(host, 80)) {
+        client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" + 
+                   "Connection: close\r\n\r\n");
+        delay(500);
 
-    if (not ok) {
-      ESP.reset();
-      delay(5000);   
+        while(client.available()) {
+          String line = client.readStringUntil('\n');
+          #ifdef DEBUG
+          Serial.println(line);
+          #endif
+          if (line.startsWith("OK")) ok = true;
+        }
+      }
+  
+      if (ok) {
+        Serial.println("OK");
+        #ifdef OLED
+        u8g2.print("OK");
+        u8g2.sendBuffer();
+        #endif
+      } else {
+        #ifdef DEBUG
+        Serial.println("FAIL");
+        #endif
+        #ifdef OLED
+        u8g2.print("FAIL");
+        u8g2.sendBuffer();
+        #endif
+      }
     }
 
     // reset the data used for averaging
     wind_num = 0;
     wind_sum = 0;
     wind_max = 0;
+    wind_min = 0;
     
     dir_num  = 0;
     dir_sum  = 0;
